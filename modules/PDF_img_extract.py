0721c55b9281c942a517f7082dda4049f2966018
Eames Lionel <bxzjshi@126.com>
2024-10-27 16:19:09 +0800

    初步完成图片信息提取的函数构建

--- modules/PDF_img_extract.py
+++ modules/PDF_img_extract.py
@@ -15,9 +15,12 @@ class PDFImage:
     pdf_filename: str
     pdf_page: int
     img_index: int
+    img_folderpath: str
     img_filename: str
     img_info: Tuple[int,int,int,int,int,str,str,str,str,int]
-    img_bytes: bytes = None
+    img_bytes: bytes 
+    img_xref: str
+    base_img: dict
     x0: int 
     y0: int 
     x1: int
@@ -31,14 +34,20 @@ class PDFExtract:
         self.pdf_page_count = self.pdf.page_count
         self.pdf_filename = os.path.basename(pdf_path)
         
+    def __enter__(self):
+        self.img_folder_path = self.createImgFolder()
+        
     def main(self) -> None: 
+        
         for page_num in range(self.pdf_page_count): 
             img_list = self.extractImgInfoList(page_num)
             page = self.pdf[page_num]
             
             for img_index, img_info in enumerate(img_list): 
                 img_filename = f"{self.pdf_filename}_page_{page_num + 1}_img_{img_index + 1}.png"
-                img = PDFImage(self.pdf_filename, page_num, img_index, img_filename, img_info)
+                img = PDFImage(self.pdf_filename, page_num, img_index, 
+                               self.img_folder_path,
+                               img_filename, img_info)
                 
                 self.extractImgInfo(page, page_num, img_index, img_info)
                 
@@ -47,13 +56,19 @@ class PDFExtract:
                                 \n\t图片尺寸过小，取消提取")
                     continue
     
-    def createImgFolder(self) -> None: 
-        """创建图片文件夹"""
+    def createImgFolder(self) -> str: 
+        """创建图片文件夹
+        
+        Returns:
+            str: 图片文件夹路径
+        """
         try:
             folder_path = os.path.join(os.path.dirname(self.pdf_path), f"{self.pdf_filename.split('-')[0]}_img")
             os.makedirs(folder_path, exist_ok=True)
+            return folder_path
         except Exception as e: 
             logger.error(f"Error: {e}\n\tpdf: {self.pdf_filename}取消创建图片文件夹")
+            return ""
         
     def extractImgInfoList(self, page_num:int)->List[Tuple[int,int,int,int,int,str,str,str,str,int]]: 
         """提取PDF某页的全部图片Xref信息
@@ -79,23 +94,36 @@ class PDFExtract:
             return []
     
     def extractImgInfo(self, page:pymupdf.Page, img:PDFImage) -> None: 
-        
+        """提取图像xref, 图像字节信息
+
+        Args:
+            page (pymupdf.Page): PDF当前页
+            img (PDFImage): 处理的当前图片
+        """
         try: 
-            img_xref = img.img_info[0]
-            base_img = self.pdf.extract_image(img_xref)
+            img.img_xref = img.img_info[0]
+            img.base_img = self.pdf.extract_image(img.img_xref)
             
-            match base_img:
+            match img.base_img:
                 case dict():
-                    img.img_bytes = base_img['image']
-                case _:
-                    logger.error(f"pdf: {img.pdf_filename}, page_num: {img.pdf_page}, img_index: {img.img_index}\
+                    img.img_bytes = img.base_img['image'] 
+                case __:
+                    logger.bug(f"pdf: {self.pdf_filename}, page_num: {img.pdf_page}, img_index: {img.img_index}\
                                 \n\tError: img类型非dict")
-                    img.img_bytes = None
+                    raise TypeError("img类型非dict")
         except Exception as e:
             logger.error(f"pdf: {self.pdf_filename}, page_num: {img.pdf_page}, img_index: {img.img_index}\
                         \n\tError: {e}")
         
+    def extractImgCoord(self, page:pymupdf.Page, img:PDFImage)->None: 
+        """提取图片坐标信息
+
+        Args:
+            page (pymupdf.Page): PDF当前页
+            img (PDFImage): 处理的当前图片
+        """
         try: 
+            img_xref = img.img_info[0]
             image_rect = page.get_image_rect(img_xref)
             rect = image_rect[0]
             img.x0, img.y0, img.x1, img.y1 = rect.x0, rect.y0, rect.x1, rect.y1
@@ -106,14 +134,27 @@ class PDFExtract:
             image_rect = None
             img.x0, img.y0, img.x1, img.y1 = None, None, None, None
             img.center_coord = None
+        except Exception as e:
+            logger.bug(f"pdf: {self.pdf_filename}, page_num: {img.pdf_page}, img_index: {img.img_index}\
+                        \n\tUnexpected BUG: {e}")
+            raise e
             
     def saveImg(self, img:PDFImage)->None:
+        """保存图片
 
+        Args:
+            img (PDFImage): 图片信息dataclass
+        """
         try: 
-            image = Image.open(io.BytesIO(img_bytes))
+            image = Image.open(io.BytesIO(img.img_bytes))
+            image_filepath = os.path.join(img.img_folderpath, img.img_filename)
+            image.save(open(image_filepath, "wb"), "PNG")
+        except Image.DecompressionBombError as e:
+            logger.error(f"pdf: {self.pdf_filename}, page_num: {img.pdf_page}, img_index: {img.img_index}\
+                        \n\t检测到过大图片,取消保存: {e}")
         except Exception as e: 
             logger.error(f"pdf: {self.pdf_filename}, page_num: {img.pdf_page}, img_index: {img.img_index}\
-                        \n\tError: {e}")
+                        \n\t保存图片失败: {e}")
             
         
             
