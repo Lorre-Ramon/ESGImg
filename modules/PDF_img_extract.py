3bf60489e2772355a91377523f739e1d67d55d98
Eames Lionel <bxzjshi@126.com>
2024-10-28 22:05:31 +0800

    优化

--- modules/PDF_img_extract.py
+++ modules/PDF_img_extract.py
@@ -32,11 +32,11 @@ class PDFImgExtract(OpenPDF):
     def __init__(self)->None: 
         super().__init__()
         
-    def main(self) -> None: 
+    def main(self) -> pd.DataFrame: 
         
         logger.info(f"pdf: {self.pdf_filename}开始提取图片")
         successful_img_cnt = 0
-        coords_df = pd.DataFrame(columns=["file_name", "x0", "y0", "x1", "y1", 
+        coords_df:pd.DataFrame = pd.DataFrame(columns=["file_name", "x0", "y0", "x1", "y1", 
                                           "centre_coordinate"])
         # 遍历PDF每一页
         for page_num in range(self.pdf_page_count): 
@@ -53,7 +53,7 @@ class PDFImgExtract(OpenPDF):
                 
                 if abs(img.x0 - img.x1) < 50 or abs(img.y0 - img.y1) < 50: 
                     logger.warning(f"pdf: {self.pdf_filename}, page_num: {page_num}, img_index: {img_index}\
-                                \n\t图片尺寸过小，取消提取")
+                                \n\t图片尺寸过小, 取消提取")
                     continue
                 
                 #TODO: 图片反色检测
@@ -72,6 +72,7 @@ class PDFImgExtract(OpenPDF):
         if successful_img_cnt < len(self.pdf) + self.global_config["successful_img_cnt_buffer"]: 
             logger.warning(f"pdf: {self.pdf_filename}提取图片数量过少, 数量: {successful_img_cnt}")
            
+        return coords_df
     
     
         
@@ -98,11 +99,10 @@ class PDFImgExtract(OpenPDF):
             logger.error(f"pdf: {self.pdf_filename}, page_num: {page_num}\n\t取消提取图片信息Error: {e}")
             return []
     
-    def extractImgInfo(self, page:pymupdf.Page, img:PDFImage) -> None: 
+    def extractImgInfo(self, img:PDFImage) -> None: 
         """提取图像xref, 图像字节信息
 
         Args:
-            page (pymupdf.Page): PDF当前页
             img (PDFImage): 处理的当前图片
         """
         try: 
@@ -177,7 +177,7 @@ class PDFImageFeatureDetect:
         """
         pass 
     
-    def isBrightness(self, img, brightness_threshold:float = 0.75)->bool: 
+    def isBright(self, img, brightness_threshold:float = 0.75) -> bool: 
         """亮度分布检测
         通过计算图像中明亮像素（灰度值大于128）的比例，来判断图像的亮度分布情况
 
@@ -195,6 +195,29 @@ class PDFImageFeatureDetect:
         brightness_flag:bool = light_fraction > brightness_threshold 
         
         return brightness_flag
+    
+    def compareColorHistogram(self, threshold:float = 0.8) -> bool:
+        pass 
+    
+    def detectEdge(self, img, edge_threshold:float = 0.1) -> bool: 
+        """边缘检测
+        通过计算图像中边缘像素的比例，来判断图像是否为边缘图像
+
+        Args:
+            img (_type_): 处理的图像
+            edge_threshold (float): 比例阈值. Defaults to 0.1.
+
+        Returns:
+            bool: True为图片为反色，False为正常
+        """
+        img_gray = img.convert('L')
+        img_gray_np: np.ndarray = np.array(img_gray)
+        
+        edges = feature.canny(img_gray_np)
+        edge_fraction = np.mean(edges)
+        edge_flag:bool = edge_fraction > edge_threshold
+        
+        return edge_flag
     
 
         
