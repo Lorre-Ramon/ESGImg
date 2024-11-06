1285778bf973ccbf336217a36a8193e18b288dcc
EamesLionel <53807755+Lorre-Ramon@users.noreply.github.com>
2024-11-06 17:25:43 +0800

    Dev/reorganize (#5)
    * 将OpenPDF的过程单独创建为一个类
    
    * 完善上下文管理器的进入退出在图像文件处理方面
    
    * 添加open_PDF的init
    
    * 移动OpenPDF类进入一个新的文件，划分文件内的类作用
    
    * 优化
    
    * 完成图像反色检测类的编写，config defaults to False
    
    * 完善图片反色检测运行逻辑，基本完成图像提取代码
    
    * 添加对手动创建data 和 log文件夹的注释
    
    * 优化图片创建文件夹的逻辑
    
    * 初步构建PDF图像提取在main中的逻辑
    
    * 修改OpenPDF类为dataclass类
    
    * 完成PDF图像提取的测试
    
    * 添加计时装饰器；添加将特殊INFO信息提交至控制台的类，未实现
    
    * 优化日志提示
    
    * 放弃在logger中添加强制在console中输出特定INFO的做法
    
    * 添加等待动画；发现使用守护线程不能在主线程结束之前结束输出
    
    * 完善装饰器
    
    * 完善docstring
    
    * 新建文本提取文件
    
    * 添加文本提取的测试文件
    
    * 明确了.get_text()为一个可用alias
    
    * finish text extract v1, waiting for debugging; Found document closed error
    
    * text_extract_debug1: finish closed Document problem
    
    * text_extract_debug: complete

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from modules import OpenPDF, PDFImgExtract
+from modules import OpenPDF
 from utils import logger, getRunTime
 
 import os 
@@ -8,10 +8,11 @@ def main(pdf_path:str) -> None:
     os.makedirs("output", exist_ok=True)
     
     with OpenPDF(pdf_path, "test_set") as pdf: 
-        extract_pdf_images(pdf)
+        extract_images(pdf)
+        extract_text(pdf)
 
 @getRunTime("提取PDF文件图片")
-def extract_pdf_images(pdf:OpenPDF) -> None: 
+def extract_images(pdf:OpenPDF) -> None: 
     """提取PDF文件中的图片
 
     Args:
@@ -38,8 +39,30 @@ def extract_pdf_images(pdf:OpenPDF) -> None:
         raise e
     finally: 
         logger.info(f"pdf: {pdf.pdf_filename}完成提取图片")
+        
+@getRunTime("提取PDF文件文本")
+def extract_text(pdf:OpenPDF) -> None: 
+    
+    text_coords_df_filepath = os.path.join("output", "text_coords.xlsx")
+    if os.path.exists(text_coords_df_filepath): 
+        text_coords_df = pd.read_excel(text_coords_df_filepath)
+    else:
+        text_coords_df = pd.DataFrame(columns=["PDF_name", "page", "p_index", 
+                                    "content", "center_x", "center_y"])
 
-
+    try: 
+        pdf_text_extract = pdf.getPDFTextExtract()
+        logger.info(f"pdf: {pdf.pdf_filename}开始提取文本")
+        text_coords_df_temp = pdf_text_extract.main()
+        text_coords_df = pd.concat([text_coords_df, text_coords_df_temp], ignore_index=True)
+        text_coords_df.to_excel(text_coords_df_filepath, index=False) 
+    except Exception as e: 
+        logger.error(f"Error: pdf: {pdf.pdf_filename}\n\t{e}")
+        raise e
+    finally: 
+        logger.info(f"pdf: {pdf.pdf_filename}完成提取文本")
+        
+        
 if __name__ == "__main__": 
     pdf_path = "data/SUS/2022/00941.HK-中国移动-中国移动 2022年度可持续发展报告-2023-03-24.pdf"
     
